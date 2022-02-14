5acc2cf2ae266d16722123d13d7097d11de7bb90
adwd <adwd@users.noreply.github.com>
2022-02-14 15:49:37 +0900

    WIP

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,13 @@
-use std::net::{IpAddr, SocketAddr, TcpListener};
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let addr = &listener.local_addr().unwrap();
+    let server = zero2prod::run(listener).expect("Failed to run server");
+    let _ = tokio::spawn(server);
+
+    format!("http://{}:{}", addr.ip(), addr.port())
+}
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
@@ -8,13 +17,13 @@ use std::net::{IpAddr, SocketAddr, TcpListener};
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    let (ip_addr, port) = spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
         // Use the returned application address
-        .get(&format!("http://{}:{}/health_check", ip_addr, port))
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -24,10 +33,58 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> (IpAddr, u16) {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
-    let addr = &listener.local_addr().unwrap();
-    let server = zero2prod::run(listener).expect("Failed to run server");
-    let _ = tokio::spawn(server);
-    (addr.ip(), addr.port())
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    println!("{:?}", response);
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        println!("{:?}", response);
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
