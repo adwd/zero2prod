043fa251995202cad4cc15be602d1e0741ed1dea
adwd <adwd@users.noreply.github.com>
2022-02-22 13:31:37 +0900

    ch6.5

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,9 @@ pub mod domain;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
+
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
